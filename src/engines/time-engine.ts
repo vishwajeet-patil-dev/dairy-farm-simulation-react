763b7d13d072556d8076e6b4963309ba8aa4a5dc
vishwajeet-patil-dev <official.vishupatil@gmail.com>
2025-07-28 19:12:46 +0530

    all the things deleted #new_beginning

--- src/engines/time-engine.ts
+++
@@ -1,6 +0,0 @@
-import { REAL_MS_IN_GAME_MINUTE } from "../constants/time";
-import { cowList } from "../game-states";
-
-setInterval(() => {
-  cowList.forEach((cow) => cow.tickMinute());
-}, [REAL_MS_IN_GAME_MINUTE]);
--- src/game-states.ts
+++
@@ -1,3 +0,0 @@
-import { Cow } from "./classes/cow";
-
-export const cowList = [new Cow("cow-1"), new Cow("cow-2")];
